c522e6068be53c8efb025ef0b14cc5ccae487096
ig momin khan <mominkhan8991@gmail.com>
2021-01-04 21:49:19 +0600

    Use of strict mode

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+"use strict";
 console.log("Hello World!");
+
+// use of "use strict" module
+
+function useOfStrict() {
+  name = "momin khan"; // use strict module will show an error while the function is called;
+}
+
+useOfStrict();
