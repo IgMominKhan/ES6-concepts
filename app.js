df223fd31bdda0f0817956c5fa693800c51f58ef
ig momin khan <mominkhan8991@gmail.com>
2021-01-04 22:13:00 +0600

    use of spread operator

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 "use strict";
 console.log("Hello World!");
 
-// use of "use strict" module
+// // use of "use strict" module
 
-function useOfStrict() {
-  name = "momin khan"; // use strict module will show an error while the function is called;
-}
+// function useOfStrict() {
+//   name = "momin khan"; // use strict module will show an error while the function is called;
+// }
 
-useOfStrict();
+// useOfStrict();
+
+// use of spread operator
+
+let poorContriesOfAsia = ["Bangladesh", "Srilanka"];
+
+let richCountriesOfAsia = ["China", "Saudi Arab", "USA"];
+
+let countriesOfAsia = [...poorContriesOfAsia, ...richCountriesOfAsia]; // spread operator (...) will add all the content of the array.
+
+console.log(countriesOfAsia);
