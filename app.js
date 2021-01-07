9eaf76a45ad12ff00fc43f9215d19e6f8c9d89b8
ig momin khan <mominkhan8991@gmail.com>
2021-01-07 22:19:19 +0600

    rest parameter in dynamic function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("Hello World!");
 // // use of "use strict" module
 
 // function useOfStrict() {
-//   name = "momin khan"; // use strict module will show an error while the function is called;
+//   name = "Momin khan"; // use strict module will show an error while the function is called;
 // }
 
 // useOfStrict();
@@ -16,13 +16,21 @@ let richCountriesOfAsia = ["China", "Saudi Arab", "USA"];
 let countriesOfAsia = [...poorCountriesOfAsia, ...richCountriesOfAsia]; // spread operator (...) will add all the content of the array.
 console.log(countriesOfAsia);
 
-// use of rest parameter
+// use of dynamic Function & rest parameter
 
-function restParameter(...elem) {
-  for (var i of elem) {
-    i += i;
+// function restParameter(...elem) {                        // rest parameter
+//   for (var i of elem) {
+//     i += i;
+//   }
+//   console.log(i);
+// }
+
+// rest parameter in dynamic function
+
+let restParameter = function (...numbers) {
+  for (var number of numbers) {
+    number += number;
   }
-  console.log(i);
-}
-
+  console.log(number);
+};
 restParameter(1, 2, 3);
